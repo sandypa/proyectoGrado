009814237508e771e95a2cfd0ec16e41c06c311d
andy645jh <andy645jh@gmail.com>
2015-08-24 23:13:58 -0500

    Subi el cambio de las rutas Agregue el modulo de autenticacion

--- formato/module/Autenticacion/config/module.config.php
+++ formato/module/Autenticacion/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Autenticacion\Controller\Index',
                         'action'     => 'index',
                     ),
                 ),
@@ -24,12 +24,12 @@ return array(
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
-            'application' => array(
+            'autenticacion' => array(
                 'type'    => 'Literal',
                 'options' => array(
-                    'route'    => '/application',
+                    'route'    => '/autenticacion',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'Autenticacion\Controller',
                         'controller'    => 'Index',
                         'action'        => 'index',
                     ),
@@ -73,7 +73,7 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Autenticacion\Controller\Index' => 'Autenticacion\Controller\IndexController'
         ),
     ),
     'view_manager' => array(
@@ -84,12 +84,12 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'autenticacion/index/index' => __DIR__ . '/../view/autenticacion/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
-           'application'=> __DIR__ . '/../view',
+           'autenticacion'=> __DIR__ . '/../view',
         ),
     ),
     // Placeholder for console routes
--- formato/module/Docentes/config/module.config.php
+++ formato/module/Docentes/config/module.config.php
@@ -31,7 +31,7 @@ return array(
                 'options' => array(
                     'route'    => '/application',
                     'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
+                        '__NAMESPACE__' => 'Docentes\Controller',
                         'controller'    => 'Index',
                         'action'        => 'index',
                     ),
@@ -82,8 +82,7 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',           
-            'docentes/index/index' => __DIR__ . '/../view/docentes/index/index.phtml',
-            'docentes/docentes/index' => __DIR__ . '/../view/docentes/docentes/index.phtml',
+          
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
